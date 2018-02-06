88db30e8e448b60deb42c3ec6e47196ff5edb199
Elscha <elscha@sse.uni-hildesheim.de>
2018-02-06 15:41:24 +0100

    Refactored Variable-Use-metric to use new AST model

--- src/net/ssehub/kernel_haven/metric_haven/metric_components/VariablesPerFunctionMetric.java
+++ src/net/ssehub/kernel_haven/metric_haven/metric_components/VariablesPerFunctionMetric.java
@@ -1,20 +1,16 @@
 package net.ssehub.kernel_haven.metric_haven.metric_components;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 import net.ssehub.kernel_haven.SetUpException;
 import net.ssehub.kernel_haven.analysis.AnalysisComponent;
-import net.ssehub.kernel_haven.code_model.SyntaxElement;
+import net.ssehub.kernel_haven.code_model.ast.Function;
 import net.ssehub.kernel_haven.config.Configuration;
 import net.ssehub.kernel_haven.config.EnumSetting;
 import net.ssehub.kernel_haven.config.Setting;
 import net.ssehub.kernel_haven.metric_haven.MetricResult;
-import net.ssehub.kernel_haven.metric_haven.filter_components.OldCodeFunction;
-import net.ssehub.kernel_haven.util.logic.Formula;
-import net.ssehub.kernel_haven.util.logic.VariableFinder;
+import net.ssehub.kernel_haven.metric_haven.filter_components.CodeFunction;
+import net.ssehub.kernel_haven.metric_haven.metric_components.visitors.UsedVariabilityVarsVisitor;
 
 /**
  * Implements the <tt>Number of internal/external configuration options</tt> metric from
@@ -41,7 +37,7 @@ public class VariablesPerFunctionMetric extends AnalysisComponent<MetricResult> 
     
     private VarType measuredVars;
     
-    private AnalysisComponent<OldCodeFunction> functionFinder;
+    private AnalysisComponent<CodeFunction> functionFinder;
     
     /**
      * Sole constructor for this class.
@@ -51,8 +47,8 @@ public class VariablesPerFunctionMetric extends AnalysisComponent<MetricResult> 
      * 
      * @throws SetUpException If {@link #VARIABLE_TYPE_PROPERTY} was defined with an invalid option.
      */
-    public VariablesPerFunctionMetric(Configuration config, AnalysisComponent<OldCodeFunction> functionFinder)
-            throws SetUpException {
+    public VariablesPerFunctionMetric(Configuration config, AnalysisComponent<CodeFunction> functionFinder)
+        throws SetUpException {
         
         super(config);
         
@@ -61,92 +57,33 @@ public class VariablesPerFunctionMetric extends AnalysisComponent<MetricResult> 
         this.functionFinder = functionFinder;
     }
 
-    /**
-     * Entry point for recursive collection of used variables. 
-     * @param function The function to measure (top level element).
-     * @param finder A visitor which is used to collect all used variables.
-     */
-    private void collectVars(SyntaxElement function, VariableFinder finder) {
-        // This formula was already visited in calc method
-        Set<Formula> formulaCache = new HashSet<>();
-        formulaCache.add(function.getCondition());
-        
-        Iterator<SyntaxElement> itr = function.iterateNestedSyntaxElements().iterator();
-        while (itr.hasNext()) {
-            SyntaxElement nestedElement = itr.next();
-            collectVars(nestedElement, finder, formulaCache);
-        }
-    }
-    
-    /**
-     * Recursive part of variable retrieval.
-     * @param astNode An arbitrary sub-element of the function to measure.
-     * @param finder A visitor which is used to collect all used variables.
-     * @param formulaCache For optimization: the list of already gathered formulas.
-     */
-    private void collectVars(SyntaxElement astNode, VariableFinder finder, Set<Formula> formulaCache) {
-        Formula formula = astNode.getCondition();
-        
-        // Optimization: search only in formula if not visited before
-        if (!formulaCache.contains(formula)) {
-            finder.visit(formula);
-            formulaCache.add(formula);
-        }
-        
-        // Recursively search in nested elements
-        Iterator<SyntaxElement> itr = astNode.iterateNestedSyntaxElements().iterator();
-        while (itr.hasNext()) {
-            SyntaxElement nestedElement = itr.next();
-            collectVars(nestedElement, finder, formulaCache);
-        }
-    }
-    
-    /**
-     * Calculates this metric for the given function AST.
-     * 
-     * @param function The AST of the function.
-     * 
-     * @return The value that this metric calculated.
-     */
-    private double calc(SyntaxElement function) {
-        VariableFinder finder = new VariableFinder();
-        finder.visit(function.getPresenceCondition());
-        Set<String> externalVariables = new HashSet<>(finder.getVariableNames());
-
-        int result;
-        switch (measuredVars) {
-        case EXTERNAL:
-            result = externalVariables.size();
-            break;
-        case INTERNAL:
-            collectVars(function, finder);
-            Set<String> allVars = new HashSet<>(finder.getVariableNames());
-            allVars.removeAll(externalVariables);
-            result = allVars.size();
-            break;
-        case ALL:
-            collectVars(function, finder);
-            result = finder.getVariableNames().size();
-            break;
-        default:
-            throw new IllegalArgumentException("Unsupported " + VarType.class.getSimpleName()
-                + " property = " + measuredVars.name());
-        }
-        
-        return result;
-    }
-    
     @Override
     protected void execute() {
-        OldCodeFunction function;
+        CodeFunction function;
         while ((function = functionFinder.getNextResult()) != null) {
+            Function functionAST = function.getFunction();
+            UsedVariabilityVarsVisitor visitor = new UsedVariabilityVarsVisitor(null);
+            functionAST.accept(visitor);
             
-            double value = calc(function.getFunction());
+            int result;
+            switch (measuredVars) {
+            case EXTERNAL:
+                result = visitor.externalVarsSize();
+                break;
+            case INTERNAL:
+                result = visitor.internalVarsSize();
+                break;
+            case ALL:
+                result = visitor.allVarsSize();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported " + VarType.class.getSimpleName()
+                    + " property = " + measuredVars.name());
+            }
             
-            SyntaxElement functionAST = function.getFunction();
             File cFile = function.getSourceFile().getPath();
             File includedFile = cFile.equals(functionAST.getSourceFile()) ? null : functionAST.getSourceFile();
-            addResult(new MetricResult(cFile, includedFile, functionAST.getLineStart(), function.getName(), value));
+            addResult(new MetricResult(cFile, includedFile, functionAST.getLineStart(), function.getName(), result));
         }
     }
     
---
+++ src/net/ssehub/kernel_haven/metric_haven/metric_components/visitors/UsedVariabilityVarsVisitor.java
@@ -0,0 +1,102 @@
+package net.ssehub.kernel_haven.metric_haven.metric_components.visitors;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.ssehub.kernel_haven.code_model.ast.CppBlock;
+import net.ssehub.kernel_haven.code_model.ast.Function;
+import net.ssehub.kernel_haven.code_model.ast.SyntaxElement;
+import net.ssehub.kernel_haven.util.logic.VariableFinder;
+import net.ssehub.kernel_haven.variability_model.VariabilityModel;
+
+/**
+ * Counts the number of  <tt>Number of internal/external configuration options</tt> as defined in
+ * <a href="https://doi.org/10.1145/2934466.2934467">
+ * Do #ifdefs influence the occurrence of vulnerabilities? an empirical study of the Linux kernel paper</a>.
+ * @author El-Sharkawy
+ *
+ */
+public class UsedVariabilityVarsVisitor extends AbstractFunctionVisitor {
+
+    private Set<String> externalVars = new HashSet<>();
+    private Set<String> internalVars = new HashSet<>();
+    private Set<String> varModelVars;
+    
+    /**
+     * Sole constructor.
+     * @param varModel Optional, if not <tt>null</tt> for each variable used in a
+     *     {@link SyntaxElement#getPresenceCondition()}, whether it is defined in the variability model.
+     */
+    public UsedVariabilityVarsVisitor(VariabilityModel varModel) {
+        super(varModel);
+        varModelVars = (null != varModel) ? Collections.unmodifiableSet(varModel.getVariableMap().keySet()) : null;
+    }
+
+    @Override
+    public void visitFunction(Function function) {
+        VariableFinder varFinder = new VariableFinder();
+        varFinder.visit(function.getPresenceCondition());
+        for (int i = varFinder.getVariableNames().size() - 1; i >= 0; i--) {
+            String symbolName = varFinder.getVariableNames().get(i);
+            if (isVarModelVariable(symbolName)) {
+                externalVars.add(symbolName);
+            }
+        }
+        
+        super.visitFunction(function);
+    }
+    
+    @Override
+    public void visitCppBlock(CppBlock block) {
+        VariableFinder varFinder = new VariableFinder();
+        varFinder.visit(block.getPresenceCondition());
+        for (int i = varFinder.getVariableNames().size() - 1; i >= 0; i--) {
+            String symbolName = varFinder.getVariableNames().get(i);
+            if (isVarModelVariable(symbolName)) {
+                internalVars.add(symbolName);
+            }
+        }
+        
+        super.visitCppBlock(block);
+    }
+    
+    /**
+     * Returns whether the specified symbol name is known by the variability model.
+     * @param symbolName The variable name to check.
+     * @return <tt>true</tt> if the variable is known by the variability model or if no model was passed to the
+     *     constructor, <tt>false</tt> otherwise.
+     */
+    private boolean isVarModelVariable(String symbolName) {
+        return (null != varModelVars) ? varModelVars.contains(symbolName) : true;
+    }
+    
+    /**
+     * Returns the number of variables used in the presence condition to include the complete function.
+     * @return The number of variables used in the presence condition to include the complete function (&gt; 0)
+     */
+    public int externalVarsSize() {
+        return externalVars.size();
+    }
+    
+    /**
+     * Returns the number of variables used inside the function.
+     * @return The number of variables used inside the function (&gt; 0)
+     */
+    public int internalVarsSize() {
+        return externalVars.size();
+    }
+    
+    
+    /**
+     * Returns the number of the union of the external and external variables.
+     * @return The number of (external variables &cup; internal variables) (&gt; 0)
+     */
+    public int allVarsSize() {
+        Set<String> superSet = new HashSet<>();
+        superSet.addAll(internalVars);
+        superSet.addAll(externalVars);
+        
+        return superSet.size();
+    }
+}
