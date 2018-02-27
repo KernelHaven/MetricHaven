c87eb2f02c3ad57da7048c51c302610281a35441
adam-sse <adam@sse.uni-hildesheim.de>
2018-02-27 14:09:07 +0100

    Make MultiMetricResult constructor public

--- src/net/ssehub/kernel_haven/metric_haven/multi_results/MultiMetricResult.java
+++ src/net/ssehub/kernel_haven/metric_haven/multi_results/MultiMetricResult.java
@@ -17,14 +17,15 @@ public class MultiMetricResult implements ITableRow {
     private @Nullable Object @NonNull [] values;
 
     /**
-     * Sole constructor, should not be visible from outside of this package.
+     * Sole constructor.
+     * 
      * @param header The header labels; first the measured item (file, included file, line number, element), afterwards
      *     the names of the measured metrics.
      * @param values The measured values (as {@link Double}s, if a value was not measured it should be <tt>null</tt>,
      *     should be as long as {@link #getHeader()} and start with the measured item
      *     (file, included file, line number, element).
      */
-    MultiMetricResult(@Nullable String @NonNull [] header, @Nullable Object @NonNull [] values) {
+    public MultiMetricResult(@Nullable String @NonNull [] header, @Nullable Object @NonNull [] values) {
         this.header = header;
         this.values = values;
     }
