80c64ceea39d9166ed180a849fd9e5980eef9012
Elscha <elscha@sse.uni-hildesheim.de>
2018-05-06 12:11:36 +0200

    TypeMeasure: Enum for creating setting.

---
+++ src/net/ssehub/kernel_haven/metric_haven/metric_components/VariabilityTypeMeasureType.java
@@ -0,0 +1,14 @@
+package net.ssehub.kernel_haven.metric_haven.metric_components;
+
+import net.ssehub.kernel_haven.variability_model.VariabilityVariable;
+
+/**
+ * Specification whether and how to incorporate types of {@link VariabilityVariable}s.
+ * @author El-Sharkawy
+ *
+ */
+public enum VariabilityTypeMeasureType {
+    NO_TYPE_MEASURING,
+    TYPE_WEIGHTS_BY_FILE;
+
+}
