990dc5370025996db5e39ac79b950fcce43efe8f
adam-sse <adam@sse.uni-hildesheim.de>
2018-10-25 14:05:55 +0200

    Adapt to refactored BuildModel

--- src/net/ssehub/kernel_haven/metric_haven/metric_components/visitors/UsedVariabilityVarsVisitor.java
+++ src/net/ssehub/kernel_haven/metric_haven/metric_components/visitors/UsedVariabilityVarsVisitor.java
@@ -68,7 +68,7 @@ public class UsedVariabilityVarsVisitor extends AbstractFunctionVisitor {
         }
         
         BuildModel bm = this.bm;
-        if (null != bm && bm.contains(function.getSourceFile())) {
+        if (null != bm && bm.containsFile(function.getSourceFile())) {
             Formula filePC = bm.getPc(function.getSourceFile());
             if (null != filePC) {
                 varFinder.clear();
